511972710143a7ded31b9db87aaffb0f0fe92b17
ruiz2894 <alvaroruiz.info@gmail.com>
2015-10-18 23:30:50 +0200

    Commit prueba bootstrap

--- index.php
+++ index.php
@@ -5,15 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Factura+</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    <h1>Hola world</h1>
-    <p>ESTO ES UNA PRUEBA DE COMMIT</p>
-    <p>Commit 3</p>
+    <h1>Factura+</h1>
+
+    <div class="row">
+        <div class="col-md-12">
+            <p>Probando bootstrap</p>
+        </div>
+    </div>
+    
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
